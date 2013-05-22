7490d62008980f8e7e5cc35e69ccdec84df8ee3a
Philip Proplesch <philip@proplesch.de>
2013-05-22 20:22:51 +0200

    Removed Web API stuff.

--- src/devplex.GitServer.Web/Global.asax.cs
+++ src/devplex.GitServer.Web/Global.asax.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Http;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace devplex.GitServer.Web
@@ -16,7 +11,6 @@ namespace devplex.GitServer.Web
         {
             AreaRegistration.RegisterAllAreas();
 
-            WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
