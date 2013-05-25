26f459fa0acbb8cce5ea44c7ba8b726c83e29dfd
Philip Proplesch <philip@proplesch.de>
2013-05-25 17:56:33 +0200

    Added some versioning stuff.

--- bld/default.ps1
+++ bld/default.ps1
@@ -5,6 +5,13 @@ Properties {
   # Directories
   $bld = Split-Path $psake.build_script_file
   $out = "$bld\..\out"
+  $src = "$bld\..\src"
+  $tmp = "$bld\..\tmp"
+
+  #Versioning
+  $major = 1
+  $minor = 0
+  $version = "$major.$minor"
 }
 
 Task Default -depends Compile
@@ -13,13 +20,49 @@ Task Compile {
   Exec { msbuild $sln /t:Build /p:Configuration=Release }
 }
 
-# TODO: Increment assembly version on build.
-Task Build -depends Clean {
-  Exec { msbuild $sln /t:Rebuild /p:Configuration=Release /p:OutDir=$out }
+Task Build -depends Clean, IncrementVersion {
+  Exec { msbuild $sln /t:Rebuild /p:Configuration=Release /p:OutDir=$tmp }
+  & "$bld\7zip\7za.exe" a -tzip $out\$script:version.zip $tmp\_PublishedWebsites\*
 }
 
 Task Clean {
-  if (Test-Path $out) {
-    Remove-Item $out -Recurse -Force
+  if ((Test-Path $out) -eq $false) {
+    New-Item $out -Type Directory
   }
+
+  if (Test-Path $tmp) {
+    Remove-Item $tmp -Recurse -Force
+  }  
+}
+
+Task IncrementVersion {
+  $build = Generate-BuildNumber
+  $revision = Generate-RevisionNumber $build
+
+  $script:version = "$major.$minor.$build.$revision"
+  
+  $assemblyVersionRegex = 'AssemblyVersion\(\".*\"\)'
+  $assemblyFileVersionRegex = 'AssemblyFileVersion\(\".*\"\)'
+  $assemblyVersion = 'AssemblyVersion("' + $script:version + '")'
+  $assemblyFileVersion = 'AssemblyFileVersion("' + $script:version + '")'
+
+  Get-ChildItem -Path $src -Recurse -Filter AssemblyInfo.cs | ForEach-Object {
+    $fileName = $_.FullName
+    
+    (Get-Content $fileName) | ForEach-Object {
+      % { $_ -replace $assemblyVersionRegex, $assemblyVersion } |
+      % { $_ -replace $assemblyFileVersionRegex, $assemblyFileVersion }
+    } | Set-Content $fileName
+  }
+}
+
+function Generate-BuildNumber {
+  $now = Get-Date
+  return (($now.Year - 2000) * 1000 + $now.DayOfYear)
+}
+
+function Generate-RevisionNumber($build) {
+  $builds = Get-ChildItem -Path "$out\$major.$minor.$build.*.zip" | Measure-Object
+  Write-Host $builds
+  return $builds.Count + 1
 }
--- src/devplex.GitServer.Web/Properties/AssemblyInfo.cs
+++ src/devplex.GitServer.Web/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("devplex.GitServer.Web")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright �  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.13145.1")]
+[assembly: AssemblyFileVersion("1.0.13145.1")]
