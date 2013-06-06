09123ee307a21cc3f880c8da78dc57f5c2672db9
Lennybacon <info@lennybacon.com>
2013-06-06 03:50:56 +0200

    F: Added Help, theme and settings for SSL

--- src/devplex.GitServer.Mvc/FrameworkExtensions/UrlHelperExtensions.cs
+++ src/devplex.GitServer.Mvc/FrameworkExtensions/UrlHelperExtensions.cs
@@ -1,18 +1,28 @@
-﻿using System.Web.Mvc;
+﻿using System.Configuration;
+using System.Web.Mvc;
 
 namespace devplex.GitServer.Mvc.FrameworkExtensions
 {
-    public static class UrlHelperExtensions
+  public static class UrlHelperExtensions
+  {
+    public static string RepositoryCloneUrl(this UrlHelper instance, string path)
     {
-         public static string RepositoryCloneUrl(this UrlHelper instance, string path)
-         {
-             var baseUrl = instance.RequestContext.HttpContext.Request.Url.AbsoluteUri;
-             if (baseUrl.EndsWith("/"))
-             {
-                 baseUrl = baseUrl.TrimEnd('/');
-             }
 
-             return string.Concat(baseUrl, path);
-         }
+      var baseUrl = instance.RequestContext.HttpContext.Request.Url.AbsoluteUri;
+      if (baseUrl.EndsWith("/"))
+      {
+        baseUrl = baseUrl.TrimEnd('/');
+      }
+
+      if ("TRUE".Equals(
+        ConfigurationManager.AppSettings["GitServer.UseSSL"],
+        System.StringComparison.OrdinalIgnoreCase) &&
+        !baseUrl.Contains("https://"))
+      {
+        baseUrl = baseUrl.Replace("http://", "https://");
+      }
+
+      return string.Concat(baseUrl, path);
     }
+  }
 }
