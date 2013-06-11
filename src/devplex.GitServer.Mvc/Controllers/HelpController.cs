203f54d2a217f3487635a5cb0ea8d5ba59210a44
Philip Proplesch <philip@d3v.biz>
2013-06-11 15:21:44 +0200

    R: Moved help to separate controller

---
+++ src/devplex.GitServer.Mvc/Controllers/HelpController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace devplex.GitServer.Mvc.Controllers
+{
+    public class HelpController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
