8636a742feb13efa326e371bff46342301bcfebf
Philip Proplesch <philip@proplesch.de>
2013-06-16 20:03:04 +0200

    R: Moved Is* methods.

---
+++ src/devplex.GitServer.Core/Extensions/RepositoryBlobExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System.Linq;
+using devplex.GitServer.Core.Models;
+
+namespace devplex.GitServer.Core.Extensions
+{
+    public static class RepositoryBlobExtensions
+    {
+        public static bool IsImage(this RepositoryBlob instance)
+        {
+            var extension = instance.FileName.ToUpperInvariant();
+            var extensions = new[] {
+                ".PNG", ".GIF", ".JPG", ".JPEG"
+            };
+
+            return extensions.Any(extension.EndsWith);
+        }
+
+        public static bool IsMarkdown(this RepositoryBlob instance)
+        {
+            var extension = instance.FileName.ToUpperInvariant();
+            var extensions = new[] {
+                ".MD", ".MARKDOWN"
+            };
+
+            return extensions.Any(extension.EndsWith);
+        }
+
+        public static bool IsBinary(this RepositoryBlob instance)
+        {
+            // TODO: Detect binary file.
+
+            var extension = instance.FileName.ToUpperInvariant();
+            var extensions = new[] {
+                ".EXE",
+                ".PPTX", ".PPT",
+                ".DOC", ".DOCX",
+                ".XLS", ".XLSX",
+                ".SNK",
+                ".ICO"
+            };
+
+            return extensions.Any(extension.EndsWith);
+        } 
+    }
+}
--- src/devplex.GitServer.Core/Models/RepositoryBlob.cs
+++ src/devplex.GitServer.Core/Models/RepositoryBlob.cs
@@ -1,46 +1,12 @@
-﻿using System.Linq;
-
-namespace devplex.GitServer.Core.Models
+﻿namespace devplex.GitServer.Core.Models
 {
     public class RepositoryBlob
     {
         public string FileName { get; set; }
+        
         public string Content { get; set; }
-
         public byte[] RawContent { get; set; }
 
         public string RawUrl { get; set; }
-
-        public bool IsImage()
-        {
-            var extension = FileName.ToUpperInvariant();
-            var extensions = new[] {".PNG", ".GIF", ".JPG", ".JPEG"};
-            
-            return extensions.Any(extension.EndsWith);
-        }
-
-        public bool IsMarkdown()
-        {
-            var extension = FileName.ToUpperInvariant();
-            var extensions = new[] { ".MD", ".MARKDOWN" };
-
-            return extensions.Any(extension.EndsWith);
-        }
-
-        public bool IsBinary()
-        {
-            // TODO: Detect binary file.
-            var extension = FileName.ToUpperInvariant();
-            var extensions = new[] {
-                ".EXE",
-                ".PPTX", ".PPT",
-                ".DOC", ".DOCX",
-                ".XLS", ".XLSX",
-                ".SNK",
-                ".ICO"
-            };
-
-            return extensions.Any(extension.EndsWith);
-        }
     }
 }
