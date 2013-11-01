b816313d50c12fb705682bf263cb18ab34771635
Daniel Fisher <info@lennybacon.com>
2013-11-01 10:48:29 +0100

    R: Worked on the help page

---
+++ src/devplex.GitServer.Mvc/Extensions/IdentityExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Security.Principal;
+
+namespace devplex.GitServer.Mvc.Extensions
+{
+  public static class IdentityExtensions
+  {
+    public static string GetUserName(this IIdentity identity)
+    {
+      var name = identity.Name;
+      var backSlashIndex = name.IndexOf("\\", StringComparison.Ordinal);
+      if (backSlashIndex > -1)
+      {
+        name = name.Substring(backSlashIndex + 1);
+      }
+      return name;
+    }
+  }
+}
